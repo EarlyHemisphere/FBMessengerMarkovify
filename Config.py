87f37424bb8a1e9231a21bd5762b7d8bce19a872
Lucas Anderson <lucasanderson1999@gmail.com>
2019-02-18 12:40:31 -0500

    Added param descriptions

--- Config.py
+++ Config.py
@@ -1,6 +1,6 @@
-email=""
-chatName=""
-messageLimit=1000
-stateSize=2
-numMarkovs=15
-numTries=1000
+email="" # FB Email
+chatName="" # The name of the group chat (case sensitive) to fetch messages from - has to be in 10-15 most recent chats
+messageLimit=1000 # The number of messages to get and train with
+stateSize=2 # The size of each state in the Markov chain
+numMarkovs=15 # The number of messages to create
+numTries=1000 # The number of tries for each message
